6053064c480dd6794a45d36b27b924aacd220732
Dhwani <dhwanijain731@gmail.com>
2025-12-10 15:06:40 +0530

    deleted irrelevant column from dataset

--- AQI_prediction_model.py
+++ AQI_prediction_model.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import numpy as np
 import joblib
-
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-
 from xgboost import XGBRegressor  
 from sklearn.ensemble import RandomForestRegressor  
+from sklearn.preprocessing import LabelEncoder
+
+# Load dataset
+df = pd.read_csv(r'D:\Projects\AQI_Prediction\Dataset\station_hour.csv')
+df = df.drop(columns=['AQI_Bucket'])
+
+
+
